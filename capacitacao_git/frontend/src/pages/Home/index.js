1d1d6a68a37705a7c9a54fa7d70689ed85029824
João Prates <jvdprates@hotmail.com>
2020-04-27 21:13:27 -0300

    Melhorando estética

--- capacitacao_git/frontend/src/pages/Home/index.js
+++ capacitacao_git/frontend/src/pages/Home/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
-export default function Home(props){
-    return(
-        <div>
-            Teste teste teste
+
+export default function Home(props) {
+    return (
+        <div className="fullPage">
+            <h1>Título da Homepage</h1>
+            <div className="yellowSquareContainer">
+                <div className="yellowSquare">
+                    <img src="lampinho.png" className="imgClass" alt="Main" />
+                </div>
+                <div className="yellowSquare">
+                    <h1>Teste teste teste</h1>
+                    <Link to='/login'>
+                        <button className='buttonText'>
+                            <h3>Fazer Login</h3>
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ capacitacao_git/frontend/src/pages/Login/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+export default function Home(props) {
+    return (
+        <div className="fullPage">
+            <h1>Título da pagina de Login</h1>
+
+            <div className="yellowSquareContainer">
+                <div className="yellowSquare">
+
+                    <h1>login login login</h1>
+
+                    <div className="yellowSquare">
+                        <h3>Nome: </h3>
+                        <input className="loginInput"></input>
+                    </div>
+                    <div className="yellowSquare">
+                        <h3>Senha: </h3>
+                        <input className="loginInput"></input>
+                    </div>
+
+                    <div className="yellowSquareContainer">
+                        <div className="yellowSquare">
+                            <Link to='/'>
+                                <button className='buttonText'>
+                                    <h3>Logar</h3>
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
