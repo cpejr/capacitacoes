a5f8c9d33643e6d1cc14d0507a2f0a42be203569
João Prates <jvdprates@hotmail.com>
2020-08-13 11:11:43 -0300

    Colocando cors

--- capacitacoes/backend_bc_solucao/src/index.js
+++ capacitacoes/backend_bc_solucao/src/index.js
@@ -6,6 +6,11 @@ const routes = require('./routes');
 const { errors } = require('celebrate');
 const app = express();
 
+const corsOptions = {
+  exposedHeaders: 'X-Total-Count',
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 app.use(errors());
